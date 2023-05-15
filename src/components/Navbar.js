4dedd4d6d88461664d7e168cb21c8b5746bc85b3
bruchimp3 <akhileshprasanna@gmail.com>
2023-05-15 15:45:41 +0530

    Mortgage Calculator using React + MUI

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
+
+const navbar = () => {
+  return (
+    <AppBar position="static">
+    <Container maxWidth = "lg">
+    <Toolbar>
+
+<Typography variant="h5" component="p" >
+  Mortgage Calculator
+</Typography>
+
+</Toolbar>
+    </Container>
+  </AppBar>
+  )
+}
+
+export default navbar
