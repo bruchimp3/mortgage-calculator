af052eec70cd6f049030a81e78c3308abfc94b07
bruchimp3 <akhileshprasanna@gmail.com>
2023-05-18 20:54:48 +0530

    useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Grid } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Result from "./components/Result";
@@ -6,13 +7,22 @@ import SliderSelect from "./components/SliderSelect";
 import TenureSelect from "./components/TenureSelect";
 
 function App() {
+  const[data,setData] = useState({
+    homeValue:3000,
+    downPayment:3000,
+    loanAmount:3000,
+    loanTerm:3000,
+  })
+
+
+
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="lg" sx={{ marginTop: 4 }}>
         <Grid container spacing = {5}>
           <Grid item xs={12} md={6}>
-            <SliderSelect />
+            <SliderSelect data={data} setData={setData} />
             <TenureSelect />
           </Grid>
           <Grid item xs={12} md={6}>
--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -1,44 +1,62 @@
 import React from "react";
 import SliderComponent from "./common/SliderComponent";
 
-
-const SliderSelect = () => {
+const SliderSelect = ({ data, setData }) => {
+  const bank_limit = 10000;
   return (
     <>
-    
       <SliderComponent
-      label="Home Value"
-        min={0}
-        max={50}
-        defaultValue={20}
-        step={10}
-        onChange={(e)=>console.log(e.target.value)}
-        unit = '$'
-        amount={3000}
+        label="Home Value"
+        min={1000}
+        max={bank_limit}
+        defaultValue={data.homeValue}
+        value={data.homeValue}
+        step={25}
+        onChange={(e, value) =>
+          setData({...data,
+            homeValue: value,
+          })
+        }
+        unit="$"
+        amount={data.homeValue}
       ></SliderComponent>
 
-<SliderComponent
-      label="Down Payment"
+      <SliderComponent
+        label="Down Payment"
         min={0}
-        max={50}
-        defaultValue={20}
-        step={10}
-        onChange={(e)=>console.log(e.target.value)}
-        unit = '$'
-        amount={500}
+        max={data.homeValue}
+        defaultValue={data.downPayment}
+        value={data.downPayment}
+        step={25}
+        onChange={(e,value) => setData({
+        
+          downPayment: value
+        })}
+        unit="$"
+        amount={data.downPayment}
       ></SliderComponent>
 
-<SliderComponent
-      label="Loan Amount"
+      <SliderComponent
+        label="Loan Amount"
         min={0}
         max={50}
         defaultValue={20}
         step={10}
-        onChange={(e)=>console.log(e.target.value)}
-        unit = '$'
+        onChange={(e) => console.log(e.target.value)}
+        unit="$"
         amount={700}
       ></SliderComponent>
-      
+
+      <SliderComponent
+        label="Interest Rate"
+        min={0}
+        max={15}
+        defaultValue={20}
+        step={10}
+        onChange={(e) => console.log(e.target.value)}
+        unit="%"
+        amount={700}
+      ></SliderComponent>
     </>
   );
 };
